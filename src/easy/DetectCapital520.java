5f5c915b51334528f9af5d7aee6c0f648e775793
Dina <451415926@qq.com>
2017-02-24 11:24:29 +0800

    DetectCapital 520 add better solution; add MaxConsecutiveOnes 485

--- src/easy/DetectCapital520.java
+++ src/easy/DetectCapital520.java
@@ -1,8 +1,9 @@
 package easy;
 
 public class DetectCapital520 {
-	public boolean detectCapitalUse(String word) {
+	/*public boolean detectCapitalUse(String word) {
         boolean isCapital = false;
+        word.matches("(a-z)+|(A-Z)+|(A-Z)");
         int lowerCount = 0;
         int upperCount = 0;
         if (word.length()==1) {
@@ -22,10 +23,27 @@ public class DetectCapital520 {
 			isCapital = true;
 		}
         return isCapital;
+    }*/
+	/*public static boolean detectCapitalUse(String word) {
+        boolean isCapital = word.matches("[a-z]+|[A-Z]+|[A-Z][a-z]+");
+        return isCapital;
+    }*/
+	
+	public static boolean detectCapitalUse(String word) {
+		if(word.length() < 2) {
+            return true;
+        }
+        if (word.toUpperCase().equals(word)) {
+			return true;
+		}
+        if (word.substring(1).toLowerCase().equals(word.substring(1))) {
+			return true;
+		}
+        return false;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(detectCapitalUse("ABCee"));
 
 	}
 
