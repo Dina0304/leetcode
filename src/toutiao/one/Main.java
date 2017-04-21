664a2a0014054631bd7dd73c0f90d92f17bf4cd5
Dina <na.di@datablau.com>
2017-04-21 20:44:31 +0800

    commit from mac

--- src/toutiao/one/Main.java
+++ src/toutiao/one/Main.java
@@ -1,19 +1,47 @@
 package toutiao.one;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
-public class Main {
+class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner cin = new Scanner(System.in);
-        int a, b;
-        while(cin.hasNextInt())
-        {
-            a = cin.nextInt();
-            b = cin.nextInt();
-            System.out.println(a + b);
-        }
-	}
 
+		int m, n;
+		Scanner scanner = new Scanner(System.in);
+		m = scanner.nextInt();
+		int[] arrayM = new int[m];
+		for (int i = 0; i < m; i++) {
+			arrayM[i] = scanner.nextInt();
+		}
+		n = scanner.nextInt();
+		int[] arrayN = new int[n];
+		for (int i = 0; i < n; i++) {
+			arrayN[i] = scanner.nextInt();
+		}
+
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < m; i++) {
+			set.add(arrayM[i]);
+		}
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			if (set.contains(arrayN[i]) == true) {
+				list.add(arrayN[i]);
+			}
+		}
+
+		// output
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i));
+			if (i < list.size() - 1) {
+				System.out.print(" ");
+			} else {
+				System.out.println();
+			}
+		}
+
+	}
 }
--- src/toutiao/two/Main.java
+++ src/toutiao/two/Main.java
@@ -1,19 +1,21 @@
 package toutiao.two;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner cin = new Scanner(System.in);
-        int a, b;
-        while(cin.hasNextInt())
-        {
-            a = cin.nextInt();
-            b = cin.nextInt();
-            System.out.println(a + b);
-        }
+		Scanner scanner = new Scanner(System.in);
+		HashSet<Long> set = new HashSet<Long>();
+		while (scanner.hasNext()) {
+			Long i = scanner.nextLong();
+			if (i == 0) {
+				break;
+			}
+			set.add(i);
+		}
+		System.out.println(set.size());
 	}
 
 }
+
